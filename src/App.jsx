e51e209d2027d6aa29a9a712f162d3f125ba0419
ItryerI <mamedbekovruslan@yandex.ru>
2025-04-22 18:33:07 +0300

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ModeChart from "./components/ModeChart";
+
+function App() {
+  return (
+    <div className="chart-wrapper">
+      <ModeChart />
+    </div>
+  );
+}
+
+export default App;
